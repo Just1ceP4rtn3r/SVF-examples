b8a9b364732056140cdcc2fedbb565e5cfd9256e
fhn <1018457919@qq.com>
2022-07-12 16:58:06 +0800

    Virtual Function test

---
+++ tests/VirtualFunction.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+class Father
+{
+    public:
+        virtual void Fun(){ cout << "Father::Fun" << endl ; }
+};
+
+class Son: public Father
+{
+    public:
+        virtual void Fun(){ cout << "Son::Fun" << endl ; }
+};
+
+int main()
+{
+    bool flag = true;//true调用Father false调用Son
+    Father *text;
+    if(flag)
+    {
+        Father father; 
+        text = &father;
+    }
+    else 
+    {
+        Son son;
+        text = &son;
+    }
+    text->Fun();
+    return 0;
+}
