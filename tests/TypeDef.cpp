db9ae6473bb42cc0a516c4f4dc8c1d27cd5a7684
Just1ceP4rtn3r <Just1ceP4rtn3r@gmail.com>
2022-07-17 22:01:35 +0800

    update

---
+++ tests/TypeDef.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+    typedef unsigned int ui;
+    ui i = 5, j = 8;
+    std::cout << "i = " << i << std::endl;
+    std::cout << "j = " << j << std::endl;
+    return 0;
+}
