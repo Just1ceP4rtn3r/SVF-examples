562dff95111bceb75b8f9990cfe1b883c3ddc556
Just1ceP4rtn3r <Just1ceP4rtn3r@gmail.com>
2022-07-18 10:22:10 +0800

    update

--- tests/Field.cpp
+++ tests/Field.cpp
@@ -2,19 +2,30 @@
 
 using namespace std;
 
-struct FIELD
+struct S1
+{
+    /* data */
+    int f;
+};
+
+struct S2
 {
 
     int a;
     int b;
+    short c;
+    double d;
+    struct S1 e;
 
 } field_test;
 
 int main()
 {
 
-    field_test.a = 10;
+    field_test.a = 1;
+    field_test.b = 2;
+    field_test.c = 2;
+    field_test.d = 2.0;
+    field_test.e = S1();
     int x = field_test.a;
 }
-
-
