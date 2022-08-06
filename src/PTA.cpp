d053fbcceddfd5b8b4251c2709965d5fc34383ef
Just1ceP4rtn3r <Just1ceP4rtn3r@gmail.com>
2022-08-06 17:09:39 +0800

    update

--- src/PTA.cpp
+++ src/PTA.cpp
@@ -75,8 +75,8 @@ namespace mqttactic
                         // else
                         //     vfCond = ComputeIntraVFGGuard(nodeBB, succBB);
 
-                        // SVFUtil::errs() << "src: " << *vNode << "\n"
-                        //                 << "dst: " << *succNode << "\n";
+                        SVFUtil::errs() << "src: " << *vNode << "\n"
+                                        << "dst: " << *succNode << "\n";
 
                         // MA node
                         if (succNode->getValue() == nullptr)
